c890db5b77be2e8f6081ef62e973fe0660ad8d51
jihyun <jihyunkkkk@nate.com>
2016-11-30 11:50:25 +0900

    modify Hello.java by jihyun Park

--- Hello_Jsp/src/com/test/hello/Hello.java
+++ Hello_Jsp/src/com/test/hello/Hello.java
@@ -4,6 +4,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Local repository create Hello_Jsp");
+		// hotfix edit by jihyun Park
+		System.out.println("Hot Fix");
 
 	}
 
